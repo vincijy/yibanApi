603bd3031b5c18dda671a2dda9ddc41d245209c8
awefight <awefight@qq.com>
2018-05-01 14:20:25 +0800

    add mc

--- yibanApi/raw_api.py
+++ yibanApi/raw_api.py
@@ -1,49 +1,100 @@
 
-from config import *
+from yibanApi.config import *
 
 import requests
 import json
+import os
+import time
+
+TOKEN_DIR = os.path.join('yibanApi', "access_token.json")
+print(TOKEN_DIR)
+
+def get_mc():
+    try:
+        with open(TOKEN_DIR) as f: return f.read()
+    except:
+        with open(TOKEN_DIR, 'w') as f: return
+
+def set_mc(j):
+    with open(TOKEN_DIR, 'w') as f: return f.write(j)
+
 
 class AccessToken(object):
 
-    def __init__(self, AppID=AppID, AppSecret=AppSecret, Redirect_uri=Redirect_uri):
+    def __init__(self, code, AppID=AppID, AppSecret=AppSecret, Redirect_uri=Redirect_uri):
         self.AppID= AppID
         self.AppSecret = AppSecret
         self.Redirect_uri = Redirect_uri
         self.__state = STATE
-        
-        self.__access_token = None
-        self.__user_id = None
-        self.__expire_time = None
+
+        self.code = code
 
         __url = BASE_URL + "oauth/authorize?client_id=%s&redirect_uri=%s&state=%s"
         self.init_url = __url % (self.AppID, self.Redirect_uri, self.__state)
 
-    def set_token(self, code):
+        #current user token
+        self.__access_token = None
+        self.__user_id = None
+        self.__end_time = "0.0"
+
+
+    def _load_file_token(self):
+        mc = get_mc()
+        if(mc and self.__user_id):
+            file_dict = json.loads(mc)
+            user_dict = file_dict[self.__user_id]
+
+            end_time = user_dict["expires"]
+            token = user_dict["access_token"]
+            #reset
+            if(end_time < time.time()):
+                self._dump_file_token()
+                return self._load_file_token()
+            return token, end_time
+
+        else:
+            self._dump_file_token()
+            return self._load_file_token()
+
+  
+
+
+
+    def _dump_file_token(self):
         url = BASE_URL + "oauth/access_token"
+        
         data = {
                 "client_id": self.AppID,
                 "client_secret": self.AppSecret, 
-                "code": code, 
+                "code": self.code, 
                 "redirect_uri":self.Redirect_uri
                 }
-        res = requests.post(url, data)
-        ctn = json.loads(res.content)
-        self.__access_token = ctn["access_token"]
+        ctn = json.loads(requests.post(url, data).content)
         self.__user_id = ctn["userid"]
-        self.__expire_time = ctn["expires"]
+        
+        mc = get_mc()
+        if(mc):
+            file_dict = json.loads(mc)
+        else:
+            file_dict = {}
+
+        file_dict[self.__user_id] = ctn
+        set_mc(json.dumps(file_dict))
+
+
+    def set_token(self):
+        self.__access_token, self.__end_time = self._load_file_token()
 
     def get_token(self):
+        if(self.__end_time < str(time.time())):
+            self.set_token()
         return self.__access_token
 
-    def access_by_token(self, procd, *args , **kwargs):
+    def access_by_token(self, procd, *args, **kwargs):
         def f(*args, **kwargs):
-            res = procd(*args, access_token=self.__access_token, **kwargs)
+            res = procd(access_token=self.get_token(), **kwargs)
             return res
         return f
-
-at = AccessToken()
-
 
 class ApiError(AttributeError):
     def __init__(self, attr):
@@ -52,36 +103,35 @@ class ApiError(AttributeError):
 
 class ApiFramework(object):
 
-    def __init__(self, attr=[]):
+    def __init__(self, attr=[], warning=False):
+
         self.attr = attr
+        self.warning = warning
 
     def __getattr__(self, s):
-        af = ApiFramework(self.attr+[s])
+        if(self.warning): raise ApiError(self.attr)
+        af = ApiFramework(self.attr+[s], warning=self.warning)
         return af
 
-    @at.access_by_token
     def __call__(self, *args, **kwargs):
         
         path = self.attr[0:-1]
         method = self.attr[-1]
         url = BASE_URL + "/".join(path)
-        print(self.attr, "===========")
-        print(method == "get")
         res_ctn =None
-        
-        # res_ctn = requests.get(url, params=kwargs).content
         if(method == "get"):
             res_ctn = requests.get(url, params=kwargs).content
         elif(method == "post"):
             res_ctn = requests.post(url, data=kwargs).content
         else:
-            raise Exception("wrong http method")
+            raise ApiError(self.attr)
         return res_ctn
         
 
+
 api = ApiFramework()
-
-
+        
+        
 
 
 
