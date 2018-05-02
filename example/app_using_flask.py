5aa192a5504235c2abef57f75bb611443a23ea25
awefight <awefight@qq.com>
2018-05-02 18:02:48 +0800

    example

--- example/app_using_flask.py
+++ example/app_using_flask.py
@@ -1,39 +1,41 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#coding=utf-8
+#获取用户信息的例子
 
-import requests
-import json
 
-from flask import Flask, request, redirect, render_template
+class Config(object):
 
-from yibanApi.raw_api import AccessToken, api
+	AppID = ""
+	AppSecret = ""
+	Redirect_uri = ""
+	STATE = "any_random_str"
+
+from flask import Flask, request, redirect
+
+from yibanApi import AccessToken, api
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    at = AccessToken(code=None)
+    at = AccessToken(code=None, Config=Config)
     return redirect(at.init_url)
 
 
 @app.route('/redirect')
-def login():
-
+def user_info():
+    
+    #获取令牌code
     __code = request.args.get("code")
-    at = AccessToken(code=__code)
 
+    #利用code 和 Config 实例化AccessToken
+    at = AccessToken(code=__code, Config=Config)
+
+    #开始使用装饰器@at.access_by_token 和api定义自己的函数
     @at.access_by_token
     def user_me(access_token):
     	return api.user.me.get(access_token=access_token)
 
-    @at.access_by_token
-    def revoke(access_token):
-    	return api.oauth.revoke_token.post(access_token=access_token, client_id=at.AppID)
-
     return user_me()
 
-
 if __name__ == '__main__':
-    
-    app.run(host="0.0.0.0", port=80)
+    app.run()
